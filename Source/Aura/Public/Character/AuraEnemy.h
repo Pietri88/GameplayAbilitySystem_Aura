9d665a52216b6cbb5e7d099b4121e4c7364cc158
Pietri88 <karol.pietras2@gmail.com>
2026-01-25 12:59:01 +0100

    2 lekcja

---
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -0,0 +1,17 @@
+// Copyright Karol Pietras
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/AuraCharacterBase.h"
+#include "AuraEnemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraEnemy : public AAuraCharacterBase
+{
+	GENERATED_BODY()
+	
+};
